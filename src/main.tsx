201587ea2c4ead4352210a7c4b9061433ee39805
yub4-arema <yub4.arema@gmail.com>
2026-09-02 19:57:18 +0900

    chore: setup React and Tailwind

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./style.css"
+
+createRoot(document.getElementById("app")!).render(
+  <StrictMode>
+    <div className="p-8">Vite + React + TypeScript + Tailwind CSS</div>
+  </StrictMode>,
+)
